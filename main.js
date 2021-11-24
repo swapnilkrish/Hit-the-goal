10400ede6c7b3aedabe99f5afd8ed882270d9787
swapnilkrish <86659449+swapnilkrish@users.noreply.github.com>
2021-11-24 15:06:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ var canvas = new fabric.Canvas('myCanvas');
 
 ball_y = 0;
 ball_x = 0;
-hole_y = 0;
-hole_x = 0;
-
+hole_y = 400;
+hole_x = 800;
 
 block_image_width = 5;
 block_image_height = 5;
@@ -44,7 +43,8 @@ function my_keydown(e) {
 	console.log(keyPressed);
 	if ((ball_x == hole_x) && (ball_y == hole_y)) {
 		canvas.remove(ball_obj);
-		document.getElementById
+		document.getElementById("hd3").innerHTML = "You have Hit the Goal | | |";
+		document.getElementById("myCanvas").style.borderColor = "red";
 	}
 	else {
 		if (keyPressed == '38') {
@@ -64,58 +64,48 @@ function my_keydown(e) {
 			console.log("right");
 		}
 	}
+}
 
-	function up() {
-		function up() {
-			if (ball_y >= 0) {
+function up() {
+	if (ball_y >= 0) {
 
-				ball_y = ball_y - block_image_height;
-				console.log("block image height = " + block_image_height);
-				console.log("when Up arrow key is pressed x = " + ball_x + " , Y =" + ball_y);
-				canvas.remove(ball_obj);
-				new_image();
-			}
-		}
+		ball_y = ball_y - block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("when Up arrow key is pressed x = " + ball_x + " , Y =" + ball_y);
+		canvas.remove(ball_obj);
+		new_image();
 	}
 }
 
 function down() {
-	function down() {
-		if (ball_y <= 450) {
+	if (ball_y <= 450) {
 
-			ball_y = ball_y + block_image_height;
-			console.log("block image height = " + block_image_height);
-			console.log("when Down arrow key is pressed x = " + ball_x + " , Y =" + ball_y);
-			canvas.remove(ball_obj);
-			new_image();
-		}
+		ball_y = ball_y + block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("when Down arrow key is pressed x = " + ball_x + " , Y =" + ball_y);
+		canvas.remove(ball_obj);
+		new_image();
 	}
 }
 
 function left() {
-	{
-		function left() {
-			if (ball_x >= 0) {
+	if (ball_x >= 0) {
 
-				ball_x = ball_x - block_image_width;
-				console.log("block image height = " + block_image_height);
-				console.log("when Left arrow key is pressed x = " + ball_x + " , Y =" + ball_y);
-				canvas.remove(ball_obj);
-				new_image();
-			}
-		}
-	}
-}
-
-function right() {
-	if (ball_x <= 1050) {
-		ball_x = ball_x + block_image_width;
-		console.log("block image height = " + block_image_height);
+		ball_x = ball_x - block_image_width;
+		console.log("block image width = " + block_image_width);
 		console.log("when Left arrow key is pressed x = " + ball_x + " , Y =" + ball_y);
 		canvas.remove(ball_obj);
 		new_image();
 	}
 }
 
-
+function right() {
+	if (ball_x <= 1050) {
+		ball_x = ball_x + block_image_width;
+		console.log("block image width = " + block_image_width);
+		console.log("when Right arrow key is pressed x = " + ball_x + " , Y =" + ball_y);
+		canvas.remove(ball_obj);
+		new_image();
+	}
+}
 
